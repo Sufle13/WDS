c5035bedac635ba10fe4416f25253c2d972d1ba0
andru1996 <43398840+andru1996@users.noreply.github.com>
2020-04-03 20:18:50 +0300

    2

--- Assets/Scenes/Scripts/FootSteps/AudioSource.cs
+++ Assets/Scenes/Scripts/FootSteps/AudioSource.cs
@@ -7,20 +7,31 @@ public class AudioSource : MonoBehaviour
 {
     AudioSource stepSource;
     AudioClip[] stepClip;
+    
+
     void Start()
     {
         stepSource = GetComponent<AudioSource>();
+        
     }
 
 
     void FixedUpdate()
     {
-        
-    }
 
+    }
+    //void Update()
+    //{
+    //    As.clip = stepClip[Random.Range(0, stepClip.Length)];
+    //    As.Play();
+    //}
+
+    //AudioSource As;
+    //AudioClip[] stepClip;
     void PlayStep(float volume)
     {
         //stepSource. = stepClip[ Random.Range(0, stepClip.Length) ];
-        //stepSource.Play();
+        
+
     }
 }
