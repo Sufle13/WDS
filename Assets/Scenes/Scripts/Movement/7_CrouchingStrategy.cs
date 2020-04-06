e6bbbb06362867437ed0ff15b3b020f623c6e8e3
sm83 <miaminights1983@yandex.ru>
2020-04-06 03:46:31 +0300

    полы, панорамирование, пар

--- Assets/Scenes/Scripts/Movement/7_CrouchingStrategy.cs
+++ Assets/Scenes/Scripts/Movement/7_CrouchingStrategy.cs
@@ -23,10 +23,11 @@
         {
             var targetScale = GetCrouchScale( character );
 
-            /*if (!character.IsInputCrouch && character.IsCrouching)
-            {
-                if (CheckCollision( character.transform, character.Capsule )) targetScale = CrouchScale; // character can not get up
-            }*/
+            //if (!character.IsInputCrouch && character.IsCrouching)
+            //{
+            //    if (CheckCollision(character.transform, character.Capsule))
+            //        targetScale = CrouchScale; // character can not get up
+            //}
 
             return targetScale;
         }
@@ -35,36 +36,35 @@
         // Helpers
         private Vector3 GetCrouchScale(CharacterPhysicsController character) 
         {
-            if (character.IsInputCrouch) return CrouchScale;
+            if (character.IsInputCrouch) 
+                return CrouchScale;
             return Vector3.one;
         }
 
-        internal static bool IsCrouching(Transform transform) 
+        internal static bool IsCrouching(Transform transform)
         {
-            return transform.localScale.y != 1f;
+            return transform.localScale.y == 0.5f;
         }
 
-        /*private static bool CheckCollision(Transform transform, CharacterController capsule)
-        {
-            var oldScale = transform.localScale;
-            transform.localScale = Vector3.one;
-            var hasCollision = CheckCollision_( transform, capsule );
-            transform.localScale = oldScale;
-            return hasCollision;
-        }
-        
+        //private static bool CheckCollision(Transform transform, CharacterController capsule)
+        //{
+        //    var oldScale = transform.localScale;
+        //    transform.localScale = Vector3.one;
+        //    var hasCollision = CheckCollision_( transform, capsule );
+        //    transform.localScale = oldScale;
+        //    return hasCollision;
+        //}
 
-        private static bool CheckCollision_(Transform transform, CharacterController capsule)
-        {
-            var sphere1 = transform.TransformPoint( capsule.center - Vector3.up * (capsule.height / 2f - capsule.radius) );
-            var sphere2 = transform.TransformPoint( capsule.center + Vector3.up * (capsule.height / 2f - capsule.radius) );
+        //private static bool CheckCollision_(Transform transform, CharacterController capsule)
+        //{
+        //    var sphere1 = transform.TransformPoint( capsule.center - Vector3.up * (capsule.height / 2f - capsule.radius) );
+        //    var sphere2 = transform.TransformPoint( capsule.center + Vector3.up * (capsule.height / 2f - capsule.radius) );
 
-            capsule.enabled = false;
-            var hasCollision = Physics.CheckCapsule( sphere1, sphere2, capsule.radius * 0.99f );
-            capsule.enabled = true;
-            return hasCollision;
-        }
-        */
+        //    capsule.enabled = false;
+        //    var hasCollision = Physics.CheckCapsule( sphere1, sphere2, capsule.radius * 0.99f );
+        //    capsule.enabled = true;
+        //    return hasCollision;
+        //}
 
 
     }
