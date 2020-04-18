b6cf062d75e7d2c7f279e4fd51635dfd06482372
sm83 <miaminights1983@yandex.ru>
2020-04-18 19:11:40 +0300

    переключатели, промежуточная работа над панорамированием (есть косяки), улица

---
+++ Assets/Scripts/Bottom/Bottom.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bottom : MonoBehaviour
+{
+    [SerializeField] GameObject bottomBlink;
+    [SerializeField] string tagObject = "Player";
+    private void OnTriggerEnter(Collider player)
+    {
+        if (player.tag.Equals(tagObject))
+        {
+            bottomBlink.active = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider player)
+    {
+        if (player.tag.Equals(tagObject))
+        {
+            bottomBlink.active = false;
+        }
+    }
+    private void OnTriggerStay(Collider player)
+    {
+        if (player.tag.Equals(tagObject))
+        {
+            if(!gameObject.GetComponent<AudioSource>().isPlaying)
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                gameObject.GetComponent<UnderSwicher>().Swich();
+            }
+        }
+    }
+}
--- Assets/Scripts/Music_Collider/Music_control.cs
+++ Assets/Scripts/Music_Collider/Music_control.cs
@@ -6,27 +6,29 @@ public class Music_control : MonoBehaviour
 {
     [SerializeField] AudioSource[] baseMusic;
     [SerializeField] AudioSource[] newMusic;
-
+    [SerializeField] float multiply = 1f;
+    [SerializeField] float standartVolumeBaseMusic =1f;
+    [SerializeField] float standartVolumeNewMusic = 0f; 
     private void OnTriggerStay(Collider player)
     {
         foreach(var music in baseMusic)
         {
-            music.volume = 1 - gameObject.GetComponent<Distance>().GetDistancePercentage();
+            music.volume = standartVolumeBaseMusic - gameObject.GetComponent<Distance>().GetDistancePercentage() * multiply;
         }
         foreach (var music in newMusic)
         {
-            music.volume = gameObject.GetComponent<Distance>().GetDistancePercentage();
+            music.volume = standartVolumeNewMusic + gameObject.GetComponent<Distance>().GetDistancePercentage() * multiply;
         }
     }
     private void OnTriggerExit(Collider player)
     {
         foreach (var music in baseMusic)
         {
-            music.volume = 1f;
+            music.volume = standartVolumeBaseMusic;
         }
         foreach (var music in newMusic)
         {
-            music.volume = 0f;
+            music.volume = standartVolumeNewMusic;
         }
     }
 }
